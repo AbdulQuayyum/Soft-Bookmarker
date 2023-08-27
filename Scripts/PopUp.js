0c6993ee311aba7e1753891d6112d443e46b1f5f
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-27 02:22:35 +0100

    Cooking, but not a Chef???????

---
+++ Scripts/PopUp.js
@@ -0,0 +1,12 @@
+// adding a new bookmark row to the popup
+const AddNewBookmark = () => { };
+
+const ViewBookmarks = () => { };
+
+const OnPlay = e => { };
+
+const OnDelete = e => { };
+
+const SetBookmarkAttributes = () => { };
+
+document.addEventListener("DOMContentLoaded", () => { });
